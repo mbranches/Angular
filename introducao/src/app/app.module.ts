a4b4673e2cf38d5f87f6501da35bc5fd3a047f77
mbranches <marcusvbranches@gmail.com>
2025-06-16 12:20:02 -0300

    refactor: substitui button do card por um component card-button

--- introducao/src/app/app.module.ts
+++ introducao/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { CardButtonComponent } from './card-button/card-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    CardButtonComponent
   ],
   imports: [
     BrowserModule
