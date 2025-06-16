a59b074e7edf914c17886b4fc34cb19db1960a50
mbranches <marcusvbranches@gmail.com>
2025-06-16 18:15:00 -0300

    feat: adiciona button cancelar ao card

--- introducao/src/app/cards/cards.module.ts
+++ introducao/src/app/cards/cards.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from '../card/card.component';
 import { CardButtonComponent } from '../card-button/card-button.component';
+import { CardButtonCancelComponent } from '../card-button-cancel/card-button-cancel.component';
 
 
 
 @NgModule({
   declarations: [
     CardComponent,
-    CardButtonComponent
+    CardButtonComponent,
+    CardButtonCancelComponent,
+    
   ],
   imports: [
     CommonModule
